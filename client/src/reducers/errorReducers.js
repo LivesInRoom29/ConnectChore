bb6e7d11543da943505b2d9bfc2209df06703106
hilbug <65197724+hilbug@users.noreply.github.com>
2020-11-12 23:36:22 -0500

    hilary add auth part 2 react and redux

---
+++ client/src/reducers/errorReducers.js
@@ -0,0 +1,13 @@
+/* eslint-disable import/no-anonymous-default-export */
+import { GET_ERRORS } from "../actions/types";
+
+const initialState = {};
+
+export default function(state = initialState, action) {
+  switch (action.type) {
+    case GET_ERRORS:
+      return action.payload;
+    default:
+      return state;
+  }
+}
---
+++ client/src/utils/setAuthToken.js
@@ -0,0 +1,18 @@
+import axios from "axios";
+
+const setAuthToken = token => {
+  if (token) {
+    // Apply authorization token to every request if logged in
+    axios.defaults.headers.common["Authorization"] = token;
+  } else {
+    // Delete auth header
+    delete axios.defaults.headers.common["Authorization"];
+  }
+};
+
+export default setAuthToken;
+
+
+
+// NOTES
+// We’ll use this to set and delete the Authorization header for our axios requests depending on whether a user is logged in or not
