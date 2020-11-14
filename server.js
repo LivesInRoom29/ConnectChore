7bf1f98e43a5988a3ef84cb32898a6ec2a8c8594
LIR29 <61219066+LivesInRoom29@users.noreply.github.com>
2020-11-14 17:11:55 -0500

    move require("dotenv") to be below require express; add poxy back to package.json

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
-require("dotenv").config();
 const express = require("express");
+require("dotenv").config();
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const app = express();
