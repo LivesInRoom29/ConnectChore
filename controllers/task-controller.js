9aa3ba8598c2e95d5d17775430b55b1d9e0fb245
LIR29 <61219066+LivesInRoom29@users.noreply.github.com>
2020-11-16 21:02:06 -0500

    Add controller and API routes for tasks

---
+++ controllers/task-controller.js
@@ -0,0 +1,16 @@
+const db = require("../models");
+
+module.exports = {
+  findAll: function() {
+    return db.Task.find();
+  },
+  findById: function(id) {
+    return db.Task.findById(id);
+  },
+  create: function(body) {
+    return db.Task.create(body);
+  },
+  update: function(id, body) {
+    return db.Task.findOneAndUpdate(id, body);
+  }
+}
