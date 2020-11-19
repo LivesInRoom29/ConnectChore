d0df71d9507e415474b398dd68353601c5c1d571
LIR29 <61219066+LivesInRoom29@users.noreply.github.com>
2020-11-18 21:30:47 -0500

    add findByUserId function to reward-descriptions controller

--- controllers/reward-description-controller.js
+++ controllers/reward-description-controller.js
@@ -4,6 +4,9 @@ module.exports = {
   findAll: function() {
     return db.RewardDescription.find();
   },
+  findByUserId: function(userid) {
+    return db.RewardDescription.find({userId: userId});
+  },
   findById: function(id) {
     return db.RewardDescription.findById(id);
   },
