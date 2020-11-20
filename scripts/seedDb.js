e84dba5cde346124c3b3a1e4b425cb66c6ca3f82
LIR29 <61219066+LivesInRoom29@users.noreply.github.com>
2020-11-20 18:28:54 -0500

    Remove import of config/keys - not needed

--- scripts/seedDb.js
+++ scripts/seedDb.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-const database = require("../config/keys");
+//const database = require("../config/keys");
 require("dotenv").config();
 ObjectId = require("mongodb").ObjectID;
 const db = require("../models");
