c5517c23545e6af50db8b3f5a7bbf243db15d013
kdunphe <57336712+kdunphe@users.noreply.github.com>
2020-11-22 17:52:02 -0500

    Updating Navbar & adding logo

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,14 +1,14 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import "./Navbar.css";
+import Logo from "../layout/connectchore.png";
 
 class Navbar extends Component {
   render() {
     return (
-
       <nav>
       <div className="nav-wrapper">
-        <Link to="/" className="brand-logo center">ConnectChore</Link>
+        <Link to="/" className="brand-logo center"><img src={ Logo }/></Link>
       </div>
     </nav>
     );
