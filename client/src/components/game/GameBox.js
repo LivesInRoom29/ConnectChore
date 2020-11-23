b85e6bfdc49963d53b13cefac4a0c9070a7b8728
Ahmed Sheikh <66457087+ASheikh-io@users.noreply.github.com>
2020-11-22 19:46:59 -0500

    still trying to set player  color in the state

--- client/src/components/game/GameBox.js
+++ client/src/components/game/GameBox.js
@@ -4,24 +4,22 @@ import React, { Component } from "react";
 import API from "../../utils/API";
 import { Container } from "react-bootstrap";
 import GridCell from "./GridCell";
-import Form from 'react-bootstrap/Form';
+import Form from "react-bootstrap/Form";
 import { dropTile } from "../../actions/gameActions";
 import "./game.css";
 
 class GameBox extends Component {
-  constructor (props){
+  constructor(props) {
     super(props);
-    this.state={
-      player1: 'red',
-      player2: 'blue',
-      currentPlayer: 'player1',
-      householdMembers: []
-    }
-  };
+    this.state = {
+      player1: "red",
+      player2: "blue",
+      currentPlayer: "player1",
+      householdMembers: [],
+    };
+  }
 
-  // setPlayerColor() {
-  //   return this.props.user.{}
-  // };
+   
 
 
   createCells() {
@@ -29,91 +27,80 @@ class GameBox extends Component {
       // <Container className="game-container">
       <div className="game-row" key={rowNum}>
         {row.map((cell, cellNum) => (
-          <GridCell color={cell.color} x={cellNum} y={rowNum} key={`${cellNum}${rowNum}`} />
+          <GridCell
+            color={cell.color}
+            x={cellNum}
+            y={rowNum}
+            key={`${cellNum}${rowNum}`}
+          />
         ))}
       </div>
       // </Container>
-      
     ));
   }
 
   componentDidMount() {
+    const { user } = this.props.auth;
 
-    const {user} = this.props.auth;
-    
     var promisetwo = new Promise((resolve, reject) => {
       API.getHouseholdMembers(user.id)
-          .then(res => resolve(res))
-          .catch(err => reject(Error("API failed")));
-  })
+        .then((res) => resolve(res))
+        .catch((err) => reject(Error("API failed")));
+    });
 
-  promisetwo.then(result => {
-      this.setState(
-          {
-              householdMembers: result.data
-          }
-      )
-  });
-
-
+    promisetwo.then((result) => {
+      this.setState({
+        householdMembers: result.data,
+      });
+    });
 
     console.log(this.props);
   }
   render() {
-
     return (
       <div>
-    <Form>
-      <Form.Row>
-        <Form.Group controlId="formHouseholdMember">
-            <Form.Label>Pick someone:</Form.Label>
-            <Form.Control
+        <Form>
+          <Form.Row>
+            <Form.Group controlId="formHouseholdMember">
+              <Form.Label>Pick someone:</Form.Label>
+              <Form.Control
                 as="select"
                 name="player1"
                 value={this.state.player1}
-                // placeholder="Wash the dishes" 
+                // placeholder="Wash the dishes"
                 onChange={this.handleInputChange}
-            >
+              >
                 {/* Map the household members to the drop-down */}
-                {
-                    this.state.householdMembers.map(member => (
-                        <option
-                            key={member._id}
-                            value={member._id}
-                        >
-                            {member.name}
-                        </option>
-                    ))
-                }
-            </Form.Control>
-        </Form.Group>
-        <Form.Group controlId="formHouseholdMember">
-            <Form.Label>Pick someone:</Form.Label>
-            <Form.Control
+                {this.state.householdMembers.map((member) => (
+                  <option key={member._id} value={member._id}>
+                    {member.name}
+                  </option>
+                ))}
+              </Form.Control>
+            </Form.Group>
+            <Form.Group controlId="formHouseholdMember">
+              <Form.Label>Pick someone:</Form.Label>
+              <Form.Control
                 as="select"
                 name="player2"
                 value={this.state.player2}
-                // placeholder="Wash the dishes" 
+                // placeholder="Wash the dishes"
                 onChange={this.handleInputChange}
-            >
+              >
                 {/* Map the household members to the drop-down */}
-                {
-                    this.state.householdMembers.map(member => (
-                        <option
-                            key={member._id}
-                            value={member._id}
-                        >
-                            {member.name}
-                        </option>
-                    ))
-                }
-            </Form.Control>
-        </Form.Group>
-      </Form.Row>
-    </Form>
+                {this.state.householdMembers.map((member) => (
+                  <option key={member._id} value={member._id}>
+                    {member.name}
+                  </option>
+                ))}
+              </Form.Control>
+            </Form.Group>
+          </Form.Row>
+        </Form>
 
-    <div>{this.createCells()}</div>
-    </div>);
+        <div>{this.createCells()}</div>
+      </div>
+    );
   }
 }
 
