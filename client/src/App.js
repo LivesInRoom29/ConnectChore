282c6629420b40045271004ef155cb2b7b1e013d
jackie <mvpbremah@yahoo.com>
2020-11-27 19:17:56 -0500

    commit
    Signed-off-by: jackie <mvpbremah@yahoo.com>

--- client/src/App.js
+++ client/src/App.js
@@ -13,7 +13,7 @@ import Landing from "./components/layout/Landing";
 import Register from "./components/auth/Register";
 import Login from "./components/auth/Login";
 import PrivateRoute from "./components/private-route/PrivateRoute";
-import Dashboard from "./components/dashboard/Dashboard";
+import dashboard from "./components/dashboard/Dashboard";
 import Rewards from "./components/rewards/RewardsForm";
 import Game from "./components/game/game";
 import Footer from "./components/footer/footer";
