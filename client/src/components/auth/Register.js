54bb040a7dcb8ff6bee39717d70bd5446d11ae27
hilbug <65197724+hilbug@users.noreply.github.com>
2020-12-02 00:50:22 -0500

    update Register.js to use Bootstrap

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -5,6 +5,13 @@ import { connect } from "react-redux";
 import { registerUser } from "../../actions/authActions";
 import classnames from "classnames";
 import "./auth.css";
+// Bootstrap
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+// Local CSS
 
 class Register extends Component {
     constructor() {
@@ -55,100 +62,121 @@ class Register extends Component {
         const { errors } = this.state;
         return (
             <>
-                <div style={{ height: "110vh" }} className="auth-container">
-                    <div>
+                <Container fluid style={{ height: "110vh" }} className="auth-container">
+                    <Row className="justify-content-center">
                         <br />
                         <br />
                         <br />
-                        <div className="col s8 loginform animation">
-                            <Link to="/" className="btn-flat waves-effect">
-                                <i className="material-icons left">keyboard_backspace</i> Back to home</Link>
-                            <div className="col s12" style={{ paddingLeft: "11.250px" }}>
+                        <Col md={5}>
+                            {/* Register Form */}
+                            <Form
+                                noValidate
+                                onSubmit={this.onSubmit}
+                                className="loginform animation mt-5"
+                            >
+                                <Link to="/" className="btn button-hover text-dark">
+                                <i className="fas fa-arrow-left"></i> Back to Home</Link>
                                 <br />
-                                <h4 className="auth">Create An Account</h4>
-                                <p className="grey-text text-darken-1">
-                                    Already have an account? <Link to="/login">Log in</Link>
+                                <h4 className="auth mt-2">Create An Account</h4>
+                                <p className="text-body">
+                                    Already have an account? <Link to="/login" className=" button-hover text-muted">Log in</Link>
                                 </p>
-                            </div>
-                            <form noValidate onSubmit={this.onSubmit}>
-                                <div className="input-field col s12">
-                                    <input
-                                        onChange={this.onChange}
-                                        value={this.state.name}
-                                        error={errors.name}
-                                        id="name"
-                                        type="text"
-                                        className={classnames("", {
-                                            invalid: errors.name
-                                        })}
-                                    />
-                                    <label htmlFor="name">Name</label>
-                                    <span className="red-text">{errors.name}</span>
-                                </div>
-                                <div className="input-field col s12">
-                                    <input
-                                        onChange={this.onChange}
-                                        value={this.state.email}
-                                        error={errors.email}
-                                        id="email"
-                                        type="email"
-                                        className={classnames("", {
-                                            invalid: errors.email
-                                        })}
-                                    />
-                                    <label htmlFor="email">Email</label>
-                                    <span className="red-text">{errors.email}</span>
-                                </div>
-                                <div className="input-field col s12">
-                                    <input
-                                        onChange={this.onChange}
-                                        value={this.state.password}
-                                        error={errors.password}
-                                        id="password"
-                                        type="password"
-                                        className={classnames("", {
-                                            invalid: errors.password
-                                        })}
-                                    />
-                                    <label htmlFor="password">Password</label>
-                                    <span className="red-text">{errors.password}</span>
-                                </div>
-                                <div className="input-field col s12">
-                                    <input
-                                        onChange={this.onChange}
-                                        value={this.state.password2}
-                                        error={errors.password2}
-                                        id="password2"
-                                        type="password"
-                                        className={classnames("", {
-                                            invalid: errors.password2
-                                        })}
-                                    />
-                                    <label htmlFor="password2">Confirm Password</label>
-                                    <span className="red-text">{errors.password2}</span>
-                                </div>
-                                <div className="col s12" style={{ paddingLeft: "11.250px" }}>
-                                    <br />
-                                    <button
-                                        style={{
-                                            width: "150px",
-                                            fontSize: "15px",
-                                            borderRadius: "30px",
-                                            backgroundColor: "#42b984",
-                                            padding: "14px",
-                                            color: "#fff",
-                                            letterSpacing: "1.5px"
-                                        }}
-                                        type="submit"
-                                        className="btn btn-large waves-effect waves-light hoverable accent-3"
+                                {/* Form Group Name */}
+                                <Form.Group as={Row} controlId="registerForm.name">
+                                    <Form.Label column sm={2}>Name</Form.Label>
+                                    <Col sm={10}>
+                                        <Form.Control 
+                                            type="text" 
+                                            placeholder="enter first and last name" 
+                                            // Previous
+                                            onChange={this.onChange}
+                                            value={this.state.name}
+                                            error={errors.name}
+                                            id="name"
+                                            className={classnames("", {
+                                                invalid: errors.name
+                                            })}
+                                        />
+                                        <span className="text-danger">{errors.name}</span>
+                                    </Col>
+                                </Form.Group>
+                                {/* Form Group Email */}
+                                <Form.Group as={Row} controlId="registerForm.email">
+                                    <Form.Label column sm={2}>Email</Form.Label>
+                                    <Col sm={10}>
+                                        <Form.Control 
+                                            type="email" 
+                                            placeholder="name@example.com" 
+                                            // Previous
+                                            onChange={this.onChange}
+                                            value={this.state.email}
+                                            error={errors.email}
+                                            id="email"
+                                            className={classnames("", {
+                                                invalid: errors.email
+                                            })}
+                                        />
+                                        <span className="text-danger">{errors.email}</span>
+                                    </Col>
+                                </Form.Group>
+                                {/* Form Group Password */}
+                                <Form.Group as={Row} controlId="registerForm.password">
+                                    <Form.Label column sm={2}>Password</Form.Label>
+                                    <Col sm={10}>
+                                        <Form.Control 
+                                            type="password"
+                                            placeholder="enter password" 
+                                            // Previous
+                                            onChange={this.onChange}
+                                            value={this.state.password}
+                                            error={errors.password}
+                                            id="password"
+                                            className={classnames("", {
+                                                invalid: errors.password
+                                            })}
+                                        />
+                                        <span className="text-danger">{errors.password}</span>
+                                    </Col>
+                                </Form.Group>
+                                {/* Form Group Password Confirm */}
+                                <Form.Group as={Row} controlId="registerForm.password2">
+                                    <Form.Label column sm={2}>Confirm password</Form.Label>
+                                    <Col sm={10}>
+                                        <Form.Control 
+                                            type="password" 
+                                            placeholder="re-enter password" 
+                                            // Previous
+                                            onChange={this.onChange}
+                                            value={this.state.password2}
+                                            error={errors.password2}
+                                            id="password2"
+                                            className={classnames("", {
+                                                invalid: errors.password2
+                                            })}
+                                        />
+                                        <span className="text-danger">{errors.password2}</span>
+                                    </Col>
+                                </Form.Group>
+                                <br />
+                                <Button
+                                    style={{
+                                        width: "150px",
+                                        fontSize: "15px",
+                                        borderRadius: "30px",
+                                        backgroundColor: "#42b984",
+                                        padding: "14px",
+                                        color: "#fff",
+                                        letterSpacing: "1.5px"
+                                    }}
+                                    type="submit"
+                                    className="btn btn-lg button-hover"
                                     >
                                         Sign up
-                                </button>
-                                </div>
-                            </form>
-                        </div>
-                    </div>
-                </div>
+                                </Button>
+                            </Form>
+                        </Col>
+                    </Row>
+                </Container>
             </>
         );
     }
