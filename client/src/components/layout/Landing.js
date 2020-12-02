f4c423adee5cecbd47e85be651c7fdac1ff4bb66
hilbug <65197724+hilbug@users.noreply.github.com>
2020-12-01 21:27:26 -0500

    refactor Landing.js to bootstrap

--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -1,5 +1,10 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
+// Bootstrap
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+// Local CSS
 import "./Landing.css";
 import "../../App.css";
 
@@ -12,60 +17,69 @@ class Landing extends Component {
 
   render() {
     return (
-      <div style={{ background: this.state.color }} id="main">
-        <div style={{ height: "90vh" }} className="landing-container">
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <div className="row">
-            <div className="col s12 center-align">
-              <h1>
+      <>
+        <Container fluid style={{ height: "100vh" }} className="landing-container" id="main">
+          {/* Header text */}
+          <Row>
+            <Col md={6} className="mx-auto">
+              <br />
+              <br />
+              <br />
+              <br />
+              <br />
+              <br />
+              <h1 className="text-center">
                 <b>Chores just got a lot more fun!</b>
               </h1>
+              <h5 className="text-center">
+                Create a list of household chores and assign tasks to family members.
+                <br />
+                Add rewards for checking items off the to-do list
+                <br />
+                Battle it out with a game of ConnectChore!
+              </h5>
+            </Col>
+          </Row>
+          {/* Buttons */}
+          <Row>
+            <Col lg={12} className="d-flex justify-content-md-center mt-5">
               <br />
-              <h5>
-                Simply create a list of household chores and assign tasks to family members. Add rewards for checking items off the to-do list and battle it out with a game of Connect Four!
-            </h5>
               <br />
-              <div className="col s6">
-                <Link
-                  to="/register"
-                  style={{
-                    width: "140px",
-                    borderRadius: "30px",
-                    padding: "14px",
-                    backgroundColor: "#42b984",
-                    color: "white",
-                    letterSpacing: "1.5px"
-                  }}
-                  className="btn btn-large waves-effect waves-light hoverable accent-3"
-                >Sign Up</Link>
-              </div>
-              <div className="col s6">
-                <Link
-                  to="/login"
-                  style={{
-                    width: "140px",
-                    borderRadius: "30px",
-                    padding: "14px",
-                    backgroundColor: "#ffffff",
-                    color: "#42b984",
-                    border: "2px solid",
-                    letterSpacing: "1.5px"
-                  }}
-                  className="btn btn-large waves-effect waves-brown waves-ripple hoverable accent-3"
-                >
-                  Log In
-              </Link>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-
+              <Row>
+                <Col lg={6}>
+                  <Link
+                    to="/register"
+                    style={{
+                      width: "140px",
+                      borderRadius: "30px",
+                      padding: "14px",
+                      backgroundColor: "#42b984",
+                      color: "white",
+                      letterSpacing: "1.5px"
+                    }}
+                    className="btn btn-lg button-hover"
+                  >Sign Up</Link>
+                </Col>
+                <Col lg={6}>
+                  <Link
+                    to="/login"
+                    style={{
+                      width: "140px",
+                      borderRadius: "30px",
+                      padding: "14px",
+                      backgroundColor: "#ffffff",
+                      color: "#42b984",
+                      border: "2px solid",
+                      letterSpacing: "1.5px"
+                    }}
+                    className="btn btn-lg button-hover"
+                  >Log In</Link>
+                </Col>
+              </Row>
+            </Col>
+          </Row>
+        </Container>
+      </>
     );
   }
 }
