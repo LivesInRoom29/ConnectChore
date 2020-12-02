8ac5cfccfd4cf4e7e6cda7e895089e66ed1e82c4
LIR29 <61219066+LivesInRoom29@users.noreply.github.com>
2020-12-01 19:32:00 -0500

    comment out redux devtools line for deployment

--- client/src/store.js
+++ client/src/store.js
@@ -13,7 +13,7 @@ const store = createStore(
     initialState,
     compose(
         applyMiddleware(...middleware),
-        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+        // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
     )
 );
 
