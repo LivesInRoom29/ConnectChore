1e29e337079d275fb7f34eba93575ee8afe3a71c
Aram2142 <andresaaramirez@aol.com>
2020-12-03 23:45:45 -0500

    all names of contributers are added to footer

--- client/src/components/footer/footer.js
+++ client/src/components/footer/footer.js
@@ -5,8 +5,14 @@ class Footer extends Component {
   render() {
     return (
       <footer>
-        <br />
-        <div>ConnectChore <a href="https://github.com/LivesInRoom29/ConnectChore" target="_blank" rel="noreferrer"><i className="fab fa-github"></i></a></div>
+        {/* <br /> */}
+        <div>ConnectChore © 2020<a href="https://github.com/LivesInRoom29/ConnectChore" target="_blank" rel="noreferrer"><i className="fab fa-github"></i></a></div>
+            <a rel="nonreferrer" href="https://github.com/hilbug">Hilary Ferraro</a>&nbsp;&#124;&nbsp;
+            <a rel="nonreferrer" href="https://github.com/LivesInRoom29">Molly Kizer</a>&nbsp;&#124;&nbsp;
+            <a rel="nonreferrer" href="https://github.com/kdunphe">Kayla Dunphe</a>&nbsp;&#124;&nbsp;
+            <a rel="nonreferrer" href="https://github.com/ARam2142">Andres Ramirez</a>&nbsp;&#124;&nbsp;
+            <a rel="nonreferrer" href="https://github.com/ASheikh-io">Ahmed Sheikh</a>&nbsp;&#124;&nbsp;
+            <a rel="nonreferrer" href="https://github.com/Bremah-mvp">Bremah Lwanga</a>
       </footer>
     );
   }
