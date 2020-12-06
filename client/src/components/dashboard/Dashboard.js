fe6939df7d1ab51ae20cc26da156a238339d2451
LIR29 <61219066+LivesInRoom29@users.noreply.github.com>
2020-12-05 20:24:36 -0500

    remove import of gamebox from dashboard; format doc.

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -7,7 +7,6 @@ import SubNav from "../layout/SubNav";
 // Bootstrap
 import { Container, Col, Row } from "react-bootstrap";
 
-import GameBox from "../game/GameBox";
 // Local CSS
 import "./Dashboard.css";
 import "../../App.css";
@@ -26,15 +25,15 @@ class Dashboard extends Component {
             <>
                 <SubNav />
                 <Container>
-                <br />
-                <br />
-                <br />
+                    <br />
+                    <br />
+                    <br />
                     <h3 className="dashboard">Hey there, {user.name.split(" ")[0]}!</h3>
                     <br />
                     <p>Get started by adding your family members and your weekly household tasks. Once you've added tasks, create chore lists for each member and assign specific tasks and rewards for finishing them! Family members can battle it out with a game of Connect Four to win the ultimate reward!</p>
                     <br />
 
-                   
+
                     <Row>
                         <Col><Link to="/addchorelist">
                             <div className="module mid animate">
@@ -66,7 +65,7 @@ class Dashboard extends Component {
                                 backgroundColor: "#42b984",
                                 color: "white",
                                 letterSpacing: "1.5px"
-                              }}
+                            }}
                         ><i className="fas fa-plus"></i>&nbsp;Add Members</Link></Col>
                         {/* <Col><button
                             style={{
@@ -89,9 +88,9 @@ class Dashboard extends Component {
                         <Col></Col>
                         <Col></Col>
                     </Row>
-                <br />
-                <br />
-                <br />
+                    <br />
+                    <br />
+                    <br />
                 </Container>
             </>
         );
