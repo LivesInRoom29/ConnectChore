ecf86486417b4e0417233f08b406b6043ae1898f
hilbug <65197724+hilbug@users.noreply.github.com>
2020-12-08 12:26:18 -0500

    make confetti random

--- client/src/components/confetti/confetti.js
+++ client/src/components/confetti/confetti.js
@@ -1,35 +1,18 @@
+/* eslint-disable import/no-anonymous-default-export */
 import React from 'react'
-// import useWindowSize from 'react-use/lib/useWindowSize'
+import useWindowSize from 'react-use/lib/useWindowSize'
 import Confetti from 'react-confetti'
- 
+
 export default () => {
-//   const { width, height } = useWindowSize()
+  const { width, height } = useWindowSize();
+  
   return (
-//     <Confetti
-//   drawShape={ctx => {
-//     ctx.beginPath()
-//     for(let i = 0; i < 22; i++) {
-//       const angle = 0.35 * i
-//       const x = (0.2 + (1 * angle))
-//       const y = (0.2 + (1 * angle))
-//       ctx.lineTo(x, y)
-//     }
-//     ctx.stroke()
-//     ctx.closePath()
-//   }}
-// />
-<Confetti
-drawShape={ctx => {
-  ctx.beginPath()
-  for(let i = 0; i < 22; i++) {
-      const angle = 0.5 * i
-      const x = (2 + (1 * angle))
-      const y = (0.5 + (1 * angle))
-    ctx.lineTo(x, y)
-  }
-  ctx.stroke()
-  ctx.closePath()
-}}
-/>
+
+    <Confetti
+      width={width}
+      height={height}
+      initialVelocityX={10}
+      initialVelocityY={10}
+    />
   )
 }
